a97b7c179b4f9adca831cb935bc9ff554cfb29b0
Noah Haselow <nhaselow@gmail.com>
2018-09-10 14:24:38 -0500

    removed incomplete schedule class

--- solver.py
+++ solver.py
@@ -16,17 +16,10 @@ class ScheduleTimeIterator :
 		if(self.time.compare(CLOSE_TIME)) :
 			self.time.set(OPEN_TIME.hours, OPEN_TIME.minutes, OPEN_TIME.isAM)
 
-
-class Schedule :
-	def __init__(self, numEmployees) :
-		self.employeeClassTimes = employeeClassTimes
-		self.schedule = [[0 for y in range(60/SHIFT_INTERVAL_LENGTH)] for x in range(rows_count)]
-
 def main() :
 	test = ScheduleTimeIterator(OPEN_TIME)
 	test.next()
 	print test.time.toString()
-	print NUM_SHIFT_INTERVALS
 	return
 
 main()
