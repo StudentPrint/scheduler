cc8d46fecc480275012010ab23b5e36d0951b297
Noah Haselow <nhaselow@gmail.com>
2018-09-12 14:55:33 -0500

    wrote tests for ScheduleTimeIterator

--- Time.py
+++ Time.py
@@ -2,8 +2,8 @@
 # minutes: int
 # isAM: bool
 class Time :
-	def __init__(self, hours, minutes, isAM) :
-		self.set(hours, minutes, isAM)
+	def __init__(self, hours, minutes) :
+		self.set(hours, minutes)
 
 	# increments time by increment
 	def next(self, increment) :
@@ -11,28 +11,27 @@ class Time :
 		if(self.minutes >= 60) :
 			self.hours += 1
 			self.minutes -= 60
-		if(self.hours >= 13) :
-			self.hours -= 12
-			self.am = not self.am
+		if(self.hours > 24) :
+			self.hours -= 24
 
 	# resets time to given value
-	def set(self, hours, minutes, isAM) :
+	def set(self, hours, minutes) :
 		self.hours = hours
 		self.minutes = minutes
-		self.isAM = isAM
 
 	# returns true if self is later than time, false o.w.
 	def compare(self, time) :
-		if(self.isAM != time.isAM) :
-			return time.isAM
-		elif(self.hours != time.hours) :
+		if(self.hours != time.hours) :
 			return self.hours > time.hours
 		else :
 			return self.minutes > time.minutes
 
 	# returns true if self equals time, false o.w.
 	def equals(self, time) :
-		return self.hours == time.hours and self.minutes == time.minutes and self.isAM == time.isAM
+		return self.hours == time.hours and self.minutes == time.minutes
 
 	def toString(self) :
-		return str(self.hours) + ':' + str(self.minutes).zfill(2) + ('AM' if self.isAM else 'PM')
+		if(self.hours > 12) :
+			return str(self.hours-12) + ':' + str(self.minutes).zfill(2) + ('AM' if self.hours == 24 else 'PM')
+		else :
+			return str(self.hours) + ':' + str(self.minutes).zfill(2) + ('PM' if self.hours == 12 else 'AM')
--- solver.py
+++ solver.py
@@ -1,25 +1,44 @@
 from Time import Time
 
-OPEN_TIME = Time(9, 15, True)
-CLOSE_TIME = Time(5, 45, True)
+OPEN_TIME = Time(9, 15)
+CLOSE_TIME = Time(17, 45)
 SHIFT_INTERVAL_LENGTH = 15
 NUM_SHIFT_INTERVALS = 34
 
 # time: Time
 class ScheduleTimeIterator :
 	def __init__(self, time) :
-		self.time = time
+		self.time = Time(time.hours, time.minutes)
 
 	# increments time by shift interval length
 	def next(self) :
 		self.time.next(SHIFT_INTERVAL_LENGTH)
 		if(self.time.compare(CLOSE_TIME)) :
-			self.time.set(OPEN_TIME.hours, OPEN_TIME.minutes, OPEN_TIME.isAM)
+			self.time.set(OPEN_TIME.hours, OPEN_TIME.minutes)
 
 def main() :
+	test()
+	return
+
+def test() :
+	passed = True
+	
+	# test time and iterator
 	test = ScheduleTimeIterator(OPEN_TIME)
 	test.next()
-	print test.time.toString()
-	return
+	if test.time.toString() != "9:30AM" :
+		passed = False 
+
+	test = ScheduleTimeIterator(Time(12, 45))
+	test.next()
+	if test.time.toString() != "1:00PM" :
+		passed = False 
+
+	test = ScheduleTimeIterator(CLOSE_TIME)
+	test.next()
+	if test.time.toString() != OPEN_TIME.toString() :
+		passed = False 
+
+	print "passed: ScheduleTimeIterator" if passed else "failed: ScheduleTimeIterator"
 
 main()
